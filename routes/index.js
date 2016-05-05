5c61ce6d39ff9ee8ac51b39c8d07a234f7133400
roy-esp <adrianroy1@hotmail.com>
2016-05-05 17:56:59 +0200

    Tema 3:Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var quiz_controller=require('../controllers/quiz_controller.js');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,6 +9,15 @@ router.get('/', function(req, res, next) {
 
 router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Author' });
+});
+
+router.get('/question', function(req, res, next) {
+  res.render('quizzes/question', { question: 'Capital de Italia' });
+});
+
+router.get('/check', function(req, res, next) {
+  var result=req.query.answer=== 'Roma' ? 'Correcta' : 'Incorrecta';
+  res.render('quizzes/result', { result: result });
 });
 
 module.exports = router;
